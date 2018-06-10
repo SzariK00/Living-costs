cf423dc6849ab38e632815e787992254b9e769a4
Adrian Jelonek <ajelonek@gmail.com>
2018-06-10 10:25:59 +0200

    Dodanie szablonu strony głównej

---
+++ wp-content/themes/basic/front-page.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by Adrian Jelonek
+ * Date: 07.06.18
+ * Time: 18:22
+ */
+get_header(); ?>
+
+<?php if (is_user_logged_in()): ?>
+    <?php $q = new WP_Query(['pagename' => 'formularz-wydatkow']) ?>
+
+    <?php if ( $q->have_posts() ) : while ( $q->have_posts() ) : $q->the_post(); ?>
+        <!-- post -->
+        <?php wp_redirect(get_permalink()); ?>
+    <?php endwhile; ?>
+        <!-- post navigation -->
+    <?php else: ?>
+        <!-- no posts found -->
+    <?php endif; ?>
+<?php else: ?>
+    <?php wp_register(); ?>
+<?php endif; ?>
+
+<?php get_footer(); ?>
