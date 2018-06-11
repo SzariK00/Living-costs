4acf8e343264e4eb83bd54a07046834c9499b43a
Adrian Jelonek <ajelonek@gmail.com>
2018-06-11 19:55:01 +0200

    Uruchomienie dodawania danych do bazy za pomocą obiektu

--- wp-content/themes/basic/functions.php
+++ wp-content/themes/basic/functions.php
@@ -4,10 +4,22 @@
  * Date: 07.06.18
  * Time: 20:21
  */
+require 'class/Expenses.php';
 
 /*First expenses form action.*/
 add_action('admin_post_add_expenses', 'process_expenses_from_user');
 
-function process_expenses_from_user() {
-    echo$_POST['user_new_expense'];
+function process_expenses_from_user()
+{
+    $currentUserId = get_current_user_id();
+    $currentUserName = get_current_user();
+    $userNewExpense = $_POST['user_new_expense'];
+    $userExpenseValue = $_POST['user_expense_value'];
+    $userExpenseDate = $_POST['user_expense_date'];
+
+    $dsn = "mysql:host=localhost;dbname=projekt";
+    $dataBaseConn = new PDO($dsn, DB_USER, DB_PASSWORD);
+
+    $addObject = new Expenses($currentUserId, $currentUserName, $userNewExpense, $userExpenseValue, $userExpenseDate);
+    $addObject->saveToDB($dataBaseConn);
 }
