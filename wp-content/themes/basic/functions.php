ffaf2ee67dd6c333636fa6c77c9bf65bb0878ac4
Adrian Jelonek <ajelonek@gmail.com>
2018-06-17 14:51:33 +0200

    Duże usprawnienie i zabezpieczenie działania usuwania wydatków z bazy

--- wp-content/themes/basic/functions.php
+++ wp-content/themes/basic/functions.php
@@ -6,6 +6,15 @@
  */
 
 /*What is needed*/
+function basic_enqueue_style()
+{
+    wp_enqueue_style('main', get_template_directory_uri() . '/css/main.css');
+    wp_enqueue_script('canvasjs', get_template_directory_uri() . '/js/canvasjs.min.js', null, null, true);
+}
+
+add_action('wp_enqueue_scripts', 'basic_enqueue_style');
+
+
 require 'class/Expenses.php';
 const DSN = 'mysql:host=localhost;dbname=projekt';
 
@@ -15,14 +24,18 @@ $dataBaseConn = new PDO(DSN, DB_USER, DB_PASSWORD);
 /*Processing add expenses form*/
 add_action('admin_post_add_expenses', 'add_expenses_to_db');
 
+/*Getting user main data*/
+$currentUserId = get_current_user_id();
+
 function add_expenses_to_db()
 {
     global $dataBaseConn;
+    global $currentUserId;
 
     /*Loading data from form*/
     if (isset($_POST['submit'])) {
-        $currentUserId = get_current_user_id();
-        $currentUserName = get_current_user();
+        $currentUser = wp_get_current_user();
+        $currentUserName = $currentUser->user_login;
         $userNewExpense = $_POST['user_new_expense'];
         $userExpenseValue = $_POST['user_expense_value'];
         $userExpenseDate = $_POST['user_expense_date'];
@@ -66,16 +79,18 @@ add_action('admin_post_delete_expenses', 'delete_expenses_from_db');
 
 function delete_expenses_from_db()
 {
+    $linkToExpensesListPage = get_permalink(get_page_by_title('zestawienie wydatkow'));
     if (isset($_POST['no'])) {
-        /*Redirecting to expenses dashboard*/
-        $showExpensesPage = get_page_by_title('zestawienie wydatkow');
-        wp_redirect(get_permalink($showExpensesPage->ID));
+        /*Redirecting to expenses list page*/
+        $noButtonValue = $_POST['no'];
+        header("Location: $linkToExpensesListPage . $noButtonValue");
     } elseif (isset($_POST['yes'])) {
         global $dataBaseConn;
+        global $currentUserId;
         $expenseToDelById = $_POST['expense_id_to_delete_from_url'];
-        Expenses::deleteExpenseFromExpensesById($dataBaseConn, $expenseToDelById);
-        /*Redirecting to expenses dashboard*/
-        $showExpensesPage = get_page_by_title('zestawienie wydatkow');
-        wp_redirect(get_permalink($showExpensesPage->ID));
+        Expenses::deleteExpenseFromExpensesById($dataBaseConn, $expenseToDelById, $currentUserId);
+        /*Redirecting to expenses list page*/
+        $yesButtonValue = $_POST['yes'];
+        header("Location: $linkToExpensesListPage . $yesButtonValue");
     }
 }
