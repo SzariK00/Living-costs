bbfc97a514403a003e617eedc4dc7c24c9cc18a4
Adrian Jelonek <ajelonek@gmail.com>
2018-06-29 01:41:43 +0200

    Oddzielenie widoku od źródła

---
+++ wp-content/themes/basic/index.view.php
@@ -0,0 +1,18 @@
+<div class="container">
+    <div class="row">
+        <div class="col header">
+            <h1 class="header__logo">E-budżet.pl</h1>
+            <h2 class="header__intro">Aby korzystać z aplikacji musisz posiadać konto i być zalogowanym.</h2>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+        <?php  wp_login_form(); ?>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <a class="btn btn-info btn-lg btn-block" href="<?php echo $registerLink; ?>">Zarejestruj się!</a>
+        </div>
+    </div>
+</div>
