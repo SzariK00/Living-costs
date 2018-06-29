5b66177bc375453e063bd8678a79a8f4a49e17bd
Adrian Jelonek <ajelonek@gmail.com>
2018-06-30 00:32:59 +0200

    Kosmetyczne zmiany w kodzie

--- wp-content/themes/basic/index.view.php
+++ wp-content/themes/basic/index.view.php
@@ -1,8 +1,8 @@
 <div class="container">
     <div class="row">
-        <div class="col header">
+        <div class="col">
             <h1 class="header__logo">E-budżet.pl</h1>
-            <h2 class="header__intro">Aby korzystać z aplikacji musisz posiadać konto i być zalogowanym.</h2>
+            <h2 class="header__intro">Aby korzystać z aplikacji musisz posiadać konto i być zalogowanym</h2>
         </div>
     </div>
     <div class="row">
@@ -12,7 +12,7 @@
     </div>
     <div class="row">
         <div class="col">
-            <a class="btn btn-info btn-lg btn-block" href="<?php echo $registerLink; ?>">Zarejestruj się!</a>
+            <a class="btn btn-info btn-lg btn-block" href="<?php echo $registerLink; ?>">Zarejestruj się</a>
         </div>
     </div>
 </div>
